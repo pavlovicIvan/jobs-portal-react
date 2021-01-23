1956675096fe748f12182b52a7d51ce4260a93d6
pavlovicIvan <ivanpavlo4@gmail.com>
2021-01-23 22:39:13 +0100

    Basic layout implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+// React
 import React from "react";
+
+// Layout
+import Header from "./layout/Header";
+
+// Components
+import SearchForm from "./components/SearchForm";
+import DisplayJobs from "./components/DisplayJobs";
+
+// Styling
 import "./App.css";
 
-const App = () => <div>Find a job</div>;
+const App = () => (
+  <>
+    <Header />
+    <SearchForm />
+    <DisplayJobs />
+  </>
+);
 
 export default App;
