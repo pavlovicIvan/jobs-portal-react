ee41d55fc3dab86e5436c4aa73772da47570bb7b
pavlovicIvan <ivanpavlo4@gmail.com>
2021-02-12 21:33:13 +0100

    Header updated

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,9 +1,29 @@
+// React
 import React from "react";
+import githubImg from "../assets/images/github.svg";
+import linkedinImg from "../assets/images/linkedin.svg";
 
 const Header = () => (
   <div className="header">
-    <div className="logo">Find me a job</div>
-    <div className="logo">Github Link</div>
+    <div className="logo">
+      Find me a <strong>job</strong>
+    </div>
+    <div className="flexContainer">
+      <a
+        href="https://github.com/pavlovicIvan"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="linkImg" alt="github logo" src={githubImg} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ivanpavlo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="linkImg" alt="github logo" src={linkedinImg} />
+      </a>
+    </div>
   </div>
 );
 
